a7dc1881e346d0c59d4aa5856b706328c076b66c
blackshadow5618 <blackshadow77.k@gmail.com>
2024-01-11 10:46:35 +0630

    updated all files

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const lightBtn = document.getElementById("light-btn");
 const darkBtn = document.getElementById("dark-btn");
 
 function darkMode(){
     document.body.classList.toggle('dark-mode');
 }
 
-lightBtn.addEventListener('click', darkMode);
 darkBtn.addEventListener('click', darkMode);
