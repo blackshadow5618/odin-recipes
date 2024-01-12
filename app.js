24e673c11d151481de07b8d175bd0b03cb3abaf9
blackshadow5618 <blackshadow77.k@gmail.com>
2024-01-12 14:21:26 +0630

    1-12-2024 updated all files

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const darkBtn = document.getElementById("dark-btn");
+const closeBtn = document.getElementById('close-btn');
+const modalImg = document.querySelector('.modal-box .body #modal-box-img');
 
+// Dark Mode 
 function darkMode(){
     document.body.classList.toggle('dark-mode');
 }
-
 darkBtn.addEventListener('click', darkMode);
+
+// Modal Box 
+closeBtn.addEventListener('click', ()=> {
+    document.querySelector('.modal-box').style.display = 'none';
+});
+
+const Images = document.querySelectorAll('img');
+// console.log(Images);
+
+Images.forEach((img)=> {
+    // console.log(img);
+
+    img.addEventListener('click', ()=>{
+        console.log(img.src);
+
+        document.querySelector('.modal-box').style.display = 'flex';
+        modalImg.src = img.src;    
+    })
+
+})
